6181834a8f34d58df4921f421ca84e5e16e81386
Md. Muttashim Mishel Mawn <k3nshi9@gmail.com>
2021-09-25 17:53:48 +0600

    adding json files for fixing vs code extention vetur error

---
+++ src/components/Results.vue
@@ -0,0 +1,37 @@
+<template>
+  <p>Reaction Time: {{ score }} ms </p>
+  <p class="rank">{{rank}}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return{
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.score < 250){
+            this.rank = 'Samurai'
+        }
+        else if(this.score < 350){
+            this.rank = 'Soldier'
+        }
+        else if(this.score < 500){
+            this.rank = 'Noob'
+        }
+        else{
+            this.rank = 'Bot'
+        }
+    },
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
